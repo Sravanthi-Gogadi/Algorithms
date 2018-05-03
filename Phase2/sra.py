f312a5d14d1a1eed3cfd8c613e5dc64809b20314
Bhargavi Nadendla <bhargavinadendla@gmail.com>
2018-05-03 12:13:15 -0500

    phase2 dijkstra's

--- Phase2/sra.py
+++ Phase2/sra.py
@@ -22,55 +22,77 @@ class Vertex(object):
     ## to help the priority queue to decide its vertex based on minDistances
 	## Here we implement the minheap using the priority queue data structure
 	# Less than method to define on what basis a minimum Node is selected
-
-    def _lt_(self, other):
-        selfPriority = self.minDistance
-        otherPriority = other.minDistance
-        return selfPriority < otherPriority
-
-
+    
+    def __lt__(self, other):
+        
+        selfPriority=self.minDistance
+        otherPriority=other.minDistance
+        return selfPriority<otherPriority
+		
+    
+    #creating an edge object with the edge's weight, start vertex and end vertex  
 class Edge(object):
-    # creating an edge object with the edge's weight, starting node
-    def _init_(self, weights, startVertex, targetVertex):
-        self.weights = weights
-        self.startVertex = startVertex
-        self.targetVertex = targetVertex
+    def __init__(self, weights, startVertex, targetVertex):
+        self.weights=weights
+        self.startVertex=startVertex
+        self.targetVertex= targetVertex
 
+	# Import Heap Functions to get the Min. Heap Root Value
+import heapq 
+      
+    # Dijkastra Algorithm Implementation  
+class Algorithm(object):    
 
-import heapq
+	# Function to Calculate the Shortest Path
+    # Requires List of Vertices.
 
-
-class Algorithm(object):
     def calculateShortestPath(self, vertexList, startVertex):
-        queue = []
-        startVertex.minDistance = 0;
-        heapq.heappush(queue, startVertex)
-
-        while len(queue) > 0:
+        queue=[]		# Implementing Heap using 1-D array
+        startVertex.minDistance = 0;       # The min Distance of the starting vertex is "0".
+        heapq.heappush(queue, startVertex)                   #initially, pushing the start vertex onto the Heap
+    
+        while len(queue)>0: # While the heap "queue" has items in it
+			# Pop the item from heap, it pops the Root Node
+            # Since, we use the lt functions to get only the minimum node,
+            # so the minimum Node is Popped out of Heap.
             actualVertex = heapq.heappop(queue)
-            for edge in actualVertex.adjacenciesList:
+            for edge in actualVertex.adjacenciesList:          
                 u = edge.startVertex
                 v = edge.targetVertex
-                newDistance = u.minDistance + edge.weights
-
-                if newDistance < v.minDistance:
+                newDistance=u.minDistance+ edge.weights      # Calculating weight
+            
+			
+			 # Since, initially all Nodes except the start node have a weight of Infinity
+             # Hence, weights will be updated based on new calculated minimum weights.
+                if newDistance< v.minDistance:
                     v.predecessor = u
                     v.minDistance = newDistance
-                    heapq.heappush(queue, v)
-
+					# Update the Heap with new weights of each node
+                    heapq.heappush(queue,v)
+    
+    
+                
     def getShortestPathTo(self, targetVertex):
-
+        # Target Vertex Node
         node = targetVertex
-        return targetVertex.minDistance, node
-
+        return targetVertex.minDistance,node
+    
+    
+    
+        
+    # Backtrack from the Target Node to the starting node using Predecessors
     def getPath(self, targetVertex):
         node = targetVertex
-        str = ""
-        while node != None:
-
-            str += node.name + "-->"
-
-            if node != None:
-                node = node.predecessor;
-        str += "You have Reached!"
+        str=""
+        while node!=None:
+                    
+                    
+                        
+           
+            str+=node.name+"-->"
+              
+            
+            if node!=None:
+                node=node.predecessor;
+        str+="You have Reached!"				
         print(str)
