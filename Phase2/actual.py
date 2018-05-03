a39e9f7fd5141e72bf5df92b56f0f8216d81d9d3
rajeswari-namana <raji.namana@gmail.com>
2018-05-03 12:26:12 -0500

    Comments added

--- Phase2/actual.py
+++ Phase2/actual.py
@@ -2,6 +2,7 @@ from sra import Vertex
 from sra import Edge
 from sra import Algorithm
 
+# Creates Vertices
 node1 = Vertex("64110")
 node2 = Vertex("64111")
 node3 = Vertex("64112")
@@ -12,6 +13,7 @@ node7 = Vertex("64116")
 node8 = Vertex("64117")
 node9 = Vertex("64118")
 
+# Creates Edges
 edge1 = Edge(2, node1, node2)
 edge2 = Edge(3, node1, node3)
 edge3 = Edge(5, node1, node4)
@@ -24,7 +26,10 @@ edge9 = Edge(5, node5, node8)
 edge10 = Edge(2, node6, node7)
 edge11 = Edge(6, node7, node9)
 
+# Taking user input
 typ = input("Enter Type of vehicle required 1. Fire 2. Ambulance 3. Police \n")
+
+# List specifying the type of vehicle, zipcode, and vehicle's availability at that particular zipcode.
 availability_list = [
     [{"type": 1, "zipcode": node1, "availability": 0}, {"type": 2, "zipcode": node1, "availability": 1},
      {"type": 3, "zipcode": node1, "availability": 0}],
@@ -44,9 +49,12 @@ availability_list = [
      {"type": 3, "zipcode": node8, "availability": 1}],
     [{"type": 1, "zipcode": node8, "availability": 1}, {"type": 2, "zipcode": node8, "availability": 2},
      {"type": 3, "zipcode": node8, "availability": 1}]]
+
+# list consisting of all the nodes
 all = []
 allnodes = [node1, node2, node3, node4, node5, node6, node7, node8, node9]
-### check for the availability of the requested type of the vehicle
+
+# checking for the availability of the requested type of the vehicle
 for v in availability_list:
     ##print(v)
     for x in v:
@@ -57,6 +65,7 @@ for v in availability_list:
             all.append(x['zipcode'])
 ##print(all)
 
+# Adding edges to adjacency list
 node1.adjacenciesList.append(edge1)
 node1.adjacenciesList.append(edge2)
 node1.adjacenciesList.append(edge3)
@@ -69,22 +78,29 @@ node5.adjacenciesList.append(edge9)
 node6.adjacenciesList.append(edge10)
 node7.adjacenciesList.append(edge11)
 
+# Creates object for algorithm class to access the method
 al = Algorithm()
+
+# Taking input from user
 zip = input("Enter the zipcode:\n")
 
+# For look  up purpose
 dic = {"64110": node1, "64111": node2, "64112": node3, "64113": node4, "64114": node5, "64115": node6, "64116": node7,
        "64117": node8, "64118": node9}
 
+# Match the requested node with the one in the dict.keys() and calculate the shortest path from requested nodes to others
 for i in dic.keys():
     if i == zip:
         al.calculateShortestPath(allnodes, dic[i])
-
+#Appending the distances of the available nodes in the form of a tuple
 dis = []
 for i in all:
     dis.append(al.getShortestPathTo(i))
 
+# Sorting using Tim Sort - an in built sort in python
 dis.sort(key=lambda tup: tup[0])
 print("The available node is at a distance of {} miles from {}".format(dis[0][0], dis[0][1].name))
 req = dis[0][1]
 
+# Sending request to the nearest availability node
 al.getPath(req)
