cd62d512a8f45cfb51e9199a06c275b681f81b61
Bhargavi Nadendla <bhargavinadendla@gmail.com>
2018-05-03 12:12:27 -0500

    phase2

--- Phase2/sra.py
+++ Phase2/sra.py
@@ -2,21 +2,26 @@ import sys
 
 ''' Create the vertex class and assigning the properties for each vertex by including the status like visited or not ,
  predecessor initially set to None and the adjacencieslist is empty list along with the minDistance of vertex set to infinity'''
-
-
 class Vertex(object):
-    ## This is the constructor that implements the class object
-    def _init_(self, name):
-        ## Assigning the name property
-        self.name = name
+    
+    def __init__(self,name):
+	## Assigning the name property
+        self.name= name
         self.visited = False
         self.predecessor = None
         self.adjacenciesList = []
-        self.minDistance = sys.maxsize  ## initialize min distance with infinity
+        self.minDistance = sys.maxsize ## initialize min distance with infinity
+		
+	# Function that defines on what basis we select a minimum Node: "minimum Distance"
+    # Required to tell the Heap that which node is the minimum Node and that is the Root
+    # Orders the nodes according to Minimum Distance before adding to the Heap.
+    # A Node is selected if the distance between Current Node and Node to be reached is smaller than other Nodes.
 
-        ## to help the priority queue to decide its vertex based on minDistances
-
-    ## Here we implement the minheap using the priority queue data structure
+    # Step where we check for smallest weight in Heap Content and select it for next step
+     
+    ## to help the priority queue to decide its vertex based on minDistances
+	## Here we implement the minheap using the priority queue data structure
+	# Less than method to define on what basis a minimum Node is selected
 
     def _lt_(self, other):
         selfPriority = self.minDistance
